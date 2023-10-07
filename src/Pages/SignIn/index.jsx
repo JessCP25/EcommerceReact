aa7bc37e730c83720a1bb8a5e72dce9166b5f175
jesspc <jpayanoc25@gmail.com>
2023-10-07 18:57:19 -0500

    Agregar el estado para mostrar la vista de crear cuenta o log in

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ShoppingCartContext } from "../../Context";
 
 const SignIn = () => {
   const {account} = useContext(ShoppingCartContext);
+  const [view, setView] = useState('user-info');
   // account
   const accountLocalStorage = localStorage.getItem('account');
   const parsedAccount = JSON.parse(accountLocalStorage);
@@ -12,10 +13,10 @@ const SignIn = () => {
   const noAccountInLocalState = account ? Object.keys(account).length === 0 : true;
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
 
-  return (
-    <div  className='w-[320px] text-center'>
-      <div className='font-medium text-lg'>Welcome</div>
-      <div className='w-full mt-7 mb-4 text-start'>
+  const renderLogIn = () => {
+    return (
+      <>
+        <div className='w-full mt-7 mb-4 text-start'>
         <p>
           <span>Email:</span>
           <span>{parsedAccount?.email}</span>
@@ -32,7 +33,24 @@ const SignIn = () => {
       </Link>
       <button className='w-full font-medium disabled:text-[#757575] disabled:border-[#757575] border-2 border-[#009688] text-[#009688] rounded-lg mb-3 py-3'
       disabled={hasUserAnAccount}
+      onClick={() => setView('create-user-info')}
       >Sign up</button>
+      </>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+    return (
+      <>Create</>
+    )
+  }
+
+  const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn();
+
+  return (
+    <div  className='w-[320px] text-center'>
+      <h1 className='font-medium text-lg'>Welcome</h1>
+      {renderView()}
     </div>
   )
 }
