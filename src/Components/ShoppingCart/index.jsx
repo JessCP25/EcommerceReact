32cedced06c43873b79115ce0370d1ba8bfb920a
jesspc <jpayanoc25@gmail.com>
2023-10-08 15:31:39 -0500

    Cambios en el diseño de ShoppingCart

--- src/Components/ShoppingCart/index.jsx
+++ src/Components/ShoppingCart/index.jsx
@@ -3,16 +3,16 @@ import { ShoppingCartIcon } from "@heroicons/react/24/solid"
 import { ShoppingCartContext } from "../../Context"
 
 const ShoppingCart = () => {
-  const {openCheckout, closeProductDetail, cartProducts} = useContext(ShoppingCartContext);
+  const {openCheckoutSideMenu, closeProductDetail, cartProducts} = useContext(ShoppingCartContext);
 
-  const openCheckoutSideMenu = () => {
-    openCheckout();
+  const openCheckout = () => {
+    openCheckoutSideMenu();
     closeProductDetail();
   }
 
   return(
     <div className='relative flex gap-0.5 items-center'
-    onClick={() => openCheckoutSideMenu()}>
+    onClick={() => openCheckout()}>
       <ShoppingCartIcon className='w-6 h-6 fill-none stroke-white cursor-pointer'/>
       <div className='absolute bottom-3.5 left-3.5 flex justify-center items-center rounded-full bg-[#009688] w-4 h-4 text-xs text-white'>
         {cartProducts.length}
